3ecfe32e7cb623147927b27ef70ff5c1d5022e3f
Bryce Klinker <bryce.klinker@gmail.com>
2016-08-24 08:47:10 -0500

    Added usage of hd (head) and tl (tail) functions

--- src/server/hello_world.exs
+++ src/server/hello_world.exs
@@ -1,5 +1,7 @@
 add = fn a, b -> a + b end
 
+list = [5, 6, 3]
+
 IO.puts "Hello World"
 
 IO.puts 6 + 4
@@ -13,3 +15,7 @@ IO.puts add.(4, 5)
 IO.puts length [1, 2, 3]
 
 IO.puts length [7, 5, 4] ++ [8, 4, 3]
+
+IO.puts hd(list)
+
+IO.puts length(tl(list))
